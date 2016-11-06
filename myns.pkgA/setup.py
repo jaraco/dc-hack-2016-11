89b8ee5afc8d3962c05747bedb4204250693d59b
Jason R. Coombs <jaraco@jaraco.com>
2016-11-06 15:07:41 -0500

    Adding sample namespace packages

---
+++ myns.pkgA/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+# Project skeleton maintained at https://github.com/jaraco/skeleton
+
+import io
+import sys
+
+import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+	long_description = readme.read()
+
+needs_wheel = {'release', 'bdist_wheel', 'dists'}.intersection(sys.argv)
+wheel = ['wheel'] if needs_wheel else []
+
+name = 'myns.pkgA'
+description = ''
+
+setup_params = dict(
+	name=name,
+	version="1.0",
+	author="Jason R. Coombs",
+	author_email="jaraco@jaraco.com",
+	description=description or name,
+	long_description=long_description,
+	url="https://github.com/jaraco/" + name,
+	packages=setuptools.find_packages(),
+	include_package_data=True,
+	namespace_packages=name.split('.')[:-1],
+	install_requires=[
+	],
+	extras_require={
+	},
+	setup_requires=[
+	] + wheel,
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3",
+	],
+	entry_points={
+	},
+)
+if __name__ == '__main__':
+	setuptools.setup(**setup_params)
